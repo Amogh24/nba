17c7c3b5d4be11233f9b9cb4ab6502abb1d34f32
Amogh24 <55030597+Amogh24@users.noreply.github.com>
2020-10-02 13:42:04 +0530

    axios for players

--- src/services/players.js
+++ src/services/players.js
@@ -1,6 +1,29 @@
 const axios = require('axios')
-const baseURL = ''
+const baseURL = 'https://free-nba.p.rapidapi.com/'
 
 const getAllPlayers=() => {
-  
+    
+
+    axios({
+        "method":"GET",
+        "url":"https://free-nba.p.rapidapi.com/players",
+        "headers":{
+        "content-type":"application/octet-stream",
+        "x-rapidapi-host":"free-nba.p.rapidapi.com",
+        "x-rapidapi-key":"13625ecbd1mshcf2097cec4a1d8fp129ce8jsnb90b82051550",
+        "useQueryString":true
+        },"params":{
+        "page":"0",
+        "per_page":"25"
+        }
+        })
+        .then((response)=>{
+          console.log(response)
+        })
+        .catch((error)=>{
+          console.log(error)
+        })
+}
+export default{
+    getAllPlayers
 }
